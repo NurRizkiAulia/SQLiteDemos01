5840cdeab438bc98f0783690898c0489306098be
NurRizkiAulia <kiicbr88@gmail.com>
2020-01-05 15:48:56 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/aulia/sqlitedemos01/DetailActivity.java
@@ -0,0 +1,102 @@
+package id.ac.poliban.aulia.sqlitedemos01;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+import id.ac.poliban.aulia.sqlitedemos01.domain.Friend;
+import id.ac.poliban.aulia.sqlitedemos01.dao.impl.FriendDaoimplSQLite;
+
+public class DetailActivity extends AppCompatActivity {
+    private FriendDaoimplSQLite db;
+    private EditText etID, etName, etAddress, etPhone, etPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        etID = findViewById(R.id.et_id);
+        etName = findViewById(R.id.et_name);
+        etAddress = findViewById(R.id.et_address);
+        etPhone = findViewById(R.id.et_phone);
+        etPhoto = findViewById(R.id.et_photo);
+        Button btSimpan = findViewById(R.id.bt_simpan);
+
+        etID.setEnabled(false);
+        
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            if (getIntent().getExtras() == null) {
+                getSupportActionBar().setTitle("Tambah");
+                getSupportActionBar().setSubtitle("aulia studios");
+            } else {
+                getSupportActionBar().setTitle("Update");
+                getSupportActionBar().setSubtitle("aulia studios");
+                populateDetail();
+            }
+        }
+        btSimpan.setOnClickListener(v -> {
+            if (etName.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Name tidak boleh kosong",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (getIntent().getExtras() == null)
+                tambahData();
+            else
+                updateData();
+        });
+    }
+
+    private void updateData() {
+        db = new FriendDaoimplSQLite(this);
+        db.update(new Friend(
+                Integer.parseInt(etID.getText().toString()),
+                etName.getText().toString(),
+                etAddress.getText().toString(),
+                etPhone.getText().toString(),
+                etPhoto.getText().toString()
+        ));
+        db.close();
+        finish();
+    }
+
+    private void tambahData() {
+        db = new FriendDaoimplSQLite(this);
+        db.insert(new Friend(
+                etName.getText().toString(),
+                etAddress.getText().toString(),
+                etPhone.getText().toString(),
+                etPhoto.getText().toString()
+        ));
+        db.close();
+        finish();
+    }
+
+    private void populateDetail() {
+        Friend friend = (Friend) getIntent().getSerializableExtra("friend");
+        etID.setText(String.valueOf(friend.getId()));
+        etName.setText(friend.getName());
+        etAddress.setText(friend.getAddress());
+        etPhone.setText(friend.getPhone());
+        etPhoto.setText(friend.getPhoto());
+        etName.requestFocus();
+    }
+
+    private void clearForm() {
+        etID.setText("");
+        etName.setText("");
+        etAddress.setText("");
+        etPhone.setText("");
+        etPhoto.setText("");
+        etName.requestFocus();
+    }
+}
